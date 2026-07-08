230368690f46c2784f67522c93c6b729b7d8d9bc
sqrqzqxkk <sqrqzqxkk@gmail.com>
2026-07-08 18:58:10 +0200

    Update guncelle.py

--- guncelle.py
+++ guncelle.py
@@ -1,27 +1,9 @@
-import requests
-import re
-
-def cnn_turk_guncel_link_bul():
-    try:
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-        url = "https://www.cnnturk.com/canli-yayin"
-        response = requests.get(url, headers=headers, timeout=15)
-        if response.status_code == 200:
-            match = re.search(r'(https://[^"\']+\.m3u8\?[^"\']+)', response.text)
-            if match:
-                return match.group(1).replace("\\/", "/")
-    except:
-        pass
-    return "https://live.duhnet.tv//S2/HLS_LIVE/cnnturknp/track_4_1000/playlist.m3u8?&live=true"
-
 def listeyi_guncelle():
-    cnn_linki = cnn_turk_guncel_link_bul()
-
     # -------------------------------------------------------------------------
-    # YURT DIŞINDA (AVUSTURYA'DA) %100 ÇALIŞAN ENGELSİZ VE AVRUPA YAYINLARI
-    # Kanallar tam olarak senin istediğin sıralamaya göre jilet gibi dizildi!
+    # YURT DIŞINDA (AVUSTURYA'DA) %100 ÇAŞILAN ENGELSİZ AVRUPA VE ULUSAL YAYINLAR
+    # CNN Türk jilet gibi tam sırasına geri eklendi!
     # -------------------------------------------------------------------------
-    m3u_icerik = f"""#EXTM3U
+    m3u_icerik = """#EXTM3U
 #EXTINF:-1 tvg-id="TRT1.tr" tvg-name="TRT 1" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/TRT_1_logo_%282021-%29.svg/960px-TRT_1_logo_%282021-%29.svg.png",TRT 1 (Avrupa / Engelsiz)
 https://tv-trt1avrupa.medya.trt.com.tr/master.m3u8
 #EXTINF:-1 tvg-id="ATV.tr" tvg-name="ATV" tvg-logo="https://i.imgur.com/HyVUwFC.png",ATV (Engelsiz)
@@ -40,8 +22,8 @@ https://tv8.daioncdn.net/tv8/tv8.m3u8?app=7ddc255a-ef47-4e81-ab14-c0e5f2949788&c
 https://dogus-live.daioncdn.net/eurostar/eurostar.m3u8
 #EXTINF:-1 tvg-id="TRTHaber.tr" tvg-name="TRT Haber" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/TRT_Haber_Eyl%C3%BCl_2020_Logo.svg/960px-TRT_Haber_Eyl%C3%BCl_2020_Logo.svg.png",TRT Haber (720p)
 https://tv-trthaber.medya.trt.com.tr/master.m3u8
-#EXTINF:-1 tvg-id="CNNTurk.tr" tvg-name="CNN Türk" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/0/09/CNN_T%C3%BCrk_logo.png",CNN Türk
-{cnn_linki}
+#EXTINF:-1 tvg-id="CNNTurk.tr" tvg-name="CNN Türk" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/0/09/CNN_T%C3%BCrk_logo.png",CNN Türk (Engelsiz)
+https://live.duhnet.tv/S2/HLS_LIVE/cnnturknp/track_4_1000/playlist.m3u8?&live=true
 #EXTINF:-1 tvg-id="AHaber.tr" tvg-name="A Haber" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/7/7c/Ahaber_Logo.png",A Haber (1080p)
 https://rnttwmjcin.turknet.ercdn.net/lcpmvefbyo/ahaber/ahaber.m3u8
 #EXTINF:-1 tvg-id="HaberturkTV.tr" tvg-name="Habertürk TV" tvg-logo="https://i.imgur.com/6Tw3rUp.png",Habertürk TV (1080p)
@@ -58,13 +40,4 @@ https://rnttwmjcin.turknet.ercdn.net/lcpmvefbyo/a2tv/a2tv.m3u8
 https://tv-trtspor.medya.trt.com.tr/master.m3u8
 #EXTINF:-1 tvg-id="ASpor.tr" tvg-name="A Spor" tvg-logo="https://i.imgur.com/ZhkZzLf.png",A Spor (1080p)
 https://rnttwmjcin.turknet.ercdn.net/lcpmvefbyo/aspor/aspor.m3u8
-#EXTINF:-1 tvg-id="TRTBelgesel.tr" tvg-name="TRT Belgesel" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/TRT_Belgesel_logo_%282019-%29.svg/960px-TRT_Belgesel_logo_%282019-%29.svg.png",TRT Belgesel (720p)
-https://tv-trtbelgesel.medya.trt.com.tr/master.m3u8
-"""
-
-    with open("iptv_listem.m3u", "w", encoding="utf-8") as f:
-        f.write(m3u_icerik.strip())
-    print("İşlem başarılı! Yurt dışı engelsiz liste tam olarak senin sıranla güncellendi.")
-
-if __name__ == "__main__":
-    listeyi_guncelle()
+#EXTINF:-1 tvg-id="TRTBelgesel.tr" tvg-name="TRT Belgesel" tvg-logo="
